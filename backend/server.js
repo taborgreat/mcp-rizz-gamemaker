ebf2e7f0c0bcc5fbd622d650ff5d9ac1a8379070
taborgreat <taborgreat@gmail.com>
2025-11-08 21:25:51 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = 8082;
 
 app.use(
   cors({
-    origin: ["https://rizz.tabors.site:5173", "http://localhost:5173"],
+    origin: ["https://rizz.tabors.site", "http://localhost:5173"],
     methods: ["GET", "POST"],
   })
 );
