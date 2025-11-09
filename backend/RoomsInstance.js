79942a746aef9e614b0b4347ce36af9eb731e28d
taborgreat <taborgreat@gmail.com>
2025-11-08 22:13:30 -0800

    update room size for production

--- backend/RoomsInstance.js
+++ backend/RoomsInstance.js
@@ -1,6 +1,6 @@
 import { Rooms } from "./core/Rooms.js";
 
-const MAX_ROOMS = 2;
-const MAX_PLAYERS_PER_ROOM = 2;
+const MAX_ROOMS = 5;
+const MAX_PLAYERS_PER_ROOM = 10;
 
 export const roomsInstance = new Rooms(MAX_ROOMS, MAX_PLAYERS_PER_ROOM);
